e4df92e3ad625c0a582dfb7de91bb9d8251d921f
Kcruiser <davydavidson777@proton.me>
2025-07-14 14:21:54 -0400

    ASDFAS

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const STATIC_URLS_TO_CACHE = [
   '/index.tsx',
   '/manifest.json',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Jost:wght@700&family=Poppins:wght@400;500&display=swap',
-  'https://i.imgur.com/3gXhe18.png', // Logo
+  'https://imgur.com/nLgleP7', // Logo
   'https://imgur.com/oDyZNFQ.jpg',   // App background
   'https://i.imgur.com/YjBbtpx.png'  // AI Avatar
 ];
